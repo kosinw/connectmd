920b674f6a7878bb0de2e3804c16383f8418b2a0
Kosi Nwabueze <kosinwabueze@gmail.com>
2020-09-19 12:18:11 -0700

    refactor(all): changed entire project structure, replaced with next.js app
    No longer using dedicated backend, moving towards firebase for rapid development.

--- packages/server/knexfile.ts
+++
@@ -1,8 +0,0 @@
-// Update with your config settings.
-import { config } from "./src/config";
-
-export default {
-  development: {
-    ...config.database,
-  },
-};
--- packages/server/src/loaders/redis.ts
+++
@@ -1,4 +0,0 @@
-import Redis from "ioredis";
-import { config } from "../config";
-
-export const redisConnection = new Redis(config.redis.port, config.redis.host);
